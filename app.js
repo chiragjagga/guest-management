ef9935447eb9663fb112f4ba5788300414b75de1
TUSHAR <tusharkhandelwal81@gmail.com>
2021-07-31 23:09:16 +0530

    Images

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({extended: true})); // New
 
-// Parse application/json
+// Parse application/json 
 // app.use(bodyParser.json());
 app.use(express.json()); // New
 
